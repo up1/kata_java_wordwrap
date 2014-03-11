ee6a800578dd3a9d92281a5d37d02f9373d8e650
Somkiat <somkiat.p@gmail.com>
2014-03-11 22:29:24 +0700

    Refactoring test to remove duplicate code

--- src/test/java/kata_wordwrap/WrapperTest.java
+++ src/test/java/kata_wordwrap/WrapperTest.java
@@ -5,24 +5,23 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class WrapperTest {
+	
+	Wrapper wrapper = new Wrapper();
 
 	@Test
 	public void emptyString() {
-		Wrapper wrapper = new Wrapper();
 		String result = wrapper.wrap("", 1);
 		assertEquals("", result);
 	}
 	
 	@Test
 	public void inputShorterThanColumn() throws Exception {
-		Wrapper wrapper = new Wrapper();
 		String result = wrapper.wrap("somkiat", 10);
 		assertEquals("somkiat", result);
 	}
 	
 	@Test
 	public void wrapTwoWordAfterSpace() throws Exception {
-		Wrapper wrapper = new Wrapper();
 		String result = wrapper.wrap("word word", 6);
 		assertEquals("word\nword", result);
 	}
