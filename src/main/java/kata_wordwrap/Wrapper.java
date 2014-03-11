86651b0bc85f3480805cdf81833301ec2bfcb473
Somkiat <somkiat.p@gmail.com>
2014-03-11 22:28:39 +0700

    Add 3rd test :: resolve

--- src/main/java/kata_wordwrap/Wrapper.java
+++ src/main/java/kata_wordwrap/Wrapper.java
@@ -3,7 +3,9 @@ package kata_wordwrap;
 public class Wrapper {
 
 	public String wrap(String input, int column) {
-		return input;
+		if(input.length() <= column)
+			return input;
+		return input.replace(" ", "\n");
 	}
 
 }
