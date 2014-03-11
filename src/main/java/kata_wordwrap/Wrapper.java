4c6f5229d8f33fa45149b6e4f69004ddbac9bc18
Somkiat <somkiat.p@gmail.com>
2014-03-11 22:13:53 +0700

    Init kata word warp

---
+++ src/main/java/kata_wordwrap/Wrapper.java
@@ -0,0 +1,9 @@
+package kata_wordwrap;
+
+public class Wrapper {
+
+	public String wrap(String string, int i) {
+		return null;
+	}
+
+}
---
+++ src/test/java/kata_wordwrap/WrapperTest.java
@@ -0,0 +1,16 @@
+package kata_wordwrap;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WrapperTest {
+
+	@Test
+	public void emptyString() {
+		Wrapper wrapper = new Wrapper();
+		String result = wrapper.wrap("", 1);
+		assertEquals("", "");
+	}
+
+}
