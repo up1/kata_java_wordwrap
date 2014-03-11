f027b5e76021163096141a41670499507bb9d850
Somkiat <somkiat.p@gmail.com>
2014-03-11 22:50:43 +0700

    Stop to resolve and use Test @Suite

--- src/main/java/kata_wordwrap/Wrapper.java
+++ src/main/java/kata_wordwrap/Wrapper.java
@@ -3,9 +3,23 @@ package kata_wordwrap;
 public class Wrapper {
 
 	public String wrap(String input, int column) {
-		if(input.length() <= column)
+		if(input.length() <= column) {
 			return input;
-		return input.replace(" ", "\n");
+		} else {
+			StringBuilder output = new StringBuilder();
+			String[] inputArray = input.split(" ");
+			int countColumn = 0;
+			for (String string : inputArray) {
+				countColumn += string.length();
+				if(countColumn< column) {
+					output.append(string).append(" ");
+				} else {
+					output.append(string+"\n");
+				}
+			}
+			return output.toString();
+		}
+		//return input.replace(" ", "\n");
 	}
 
 }
--- src/test/java/kata_wordwrap/WrapperTest.java
+++ src/test/java/kata_wordwrap/WrapperTest.java
@@ -3,39 +3,53 @@ package kata_wordwrap;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  WrapperTest.DegenerateTests.class
+})
 public class WrapperTest {
-	
-	Wrapper wrapper = new Wrapper();
 
-	@Test
-	public void emptyString() {
-		String result = wrapper.wrap("", 1);
-		assertEquals("", result);
+	public static class DegenerateTests {
+		Wrapper wrapper = new Wrapper();
+
+		@Test
+		public void emptyString() {
+			String result = wrapper.wrap("", 1);
+			assertEquals("", result);
+		}
+
+		@Test
+		public void inputShorterThanColumn() throws Exception {
+			String result = wrapper.wrap("somkiat", 10);
+			assertEquals("somkiat", result);
+		}
+
 	}
+
 	
-	@Test
-	public void inputShorterThanColumn() throws Exception {
-		String result = wrapper.wrap("somkiat", 10);
-		assertEquals("somkiat", result);
-	}
-	
-	@Test
-	public void wrapTwoWordAfterSpace() throws Exception {
-		String result = wrapper.wrap("word word", 6);
-		assertEquals("word\nword", result);
-	}
-	
-	@Test
-	public void wrapThreeWordAfterFirstSpace() throws Exception {
-		String result = wrapper.wrap("word word word", 6);
-		assertEquals("word\nword\nword", result);
-	}
-	
-	@Test
-	public void wrapThreeWordsAfterSecondSpace() throws Exception {
-		String result = wrapper.wrap("word word word", 11);
-		assertEquals("word word\nword", result);
+	public static class wrapWordsTest {
+		Wrapper wrapper = new Wrapper();
+		
+		@Test
+		public void wrapTwoWordAfterSpace() throws Exception {
+			String result = wrapper.wrap("word word", 6);
+			assertEquals("word\nword", result);
+		}
+
+		@Test
+		public void wrapThreeWordAfterFirstSpace() throws Exception {
+			String result = wrapper.wrap("word word word", 6);
+			assertEquals("word\nword\nword", result);
+		}
+
+		@Test
+		public void wrapThreeWordsAfterSecondSpace() throws Exception {
+			String result = wrapper.wrap("word word word", 11);
+			assertEquals("word word\nword", result);
+		}
 	}
 
 }
